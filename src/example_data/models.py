a333bd76ca5ad3ead5b5d8aa5a384f50a3e1b811
Mikko <mikko.vaaranmaki@gmail.com>
2018-03-14 22:52:11 +0200

    Datamodel TrafficLight added

---
+++ src/example_data/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import date
+
+class TrafficLigt(models.Model):
+    name = models.TextField()
+    street = models.TextField(blank=True, null=True)
+    number = models.IntegerField(blank=True, null=True)
+    identifier = models.TextField(default="")
+    state = models.NullBooleanField(blank=True, null=True, default=None)
+    last_updated = models.DateField(default=None)
+    coordinate_x = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=6)
+    coordinate_y = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=6)
+
+    def __unicode__(self):
+        return u"%s" % self.name
