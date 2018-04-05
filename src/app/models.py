059650012983a121d73c1f733e91628f759b60df
Mikko <mikko.vaaranmaki@gmail.com>
2018-04-05 22:40:58 +0300

    ValueError: invalid literal for int() with base 10: ''

--- src/app/models.py
+++ src/app/models.py
@@ -5,10 +5,13 @@ from datetime import date
 #Class for TrafficLight object
 class TrafficLight(models.Model):
     name = models.TextField()
-    street = models.TextField(blank=True, null=True)
-    number = models.IntegerField(blank=True, null=True)
-    identifier = models.IntegerField(default="")
-    state = models.NullBooleanField(blank=True, null=True, default=None)
-    last_updated = models.DateField(default=None)
+    last_updated = models.DateField(blank=True, null=True)
+    crossing = models.TextField(blank=True, null=True)
+    idx = models.IntegerField(default="")
+    status = models.TextField(default="")
+#    status = models.NullBooleanField(blank=True, null=True, default=None)
     coordinate_x = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=6)
     coordinate_y = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=6)
+
+#    def __str__(self):
+#        return self.name
--- src/app/urls.py
+++ src/app/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from .views import fetch_status
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
-    path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
+    path('update/', fetch_status, name='update')
 ]
