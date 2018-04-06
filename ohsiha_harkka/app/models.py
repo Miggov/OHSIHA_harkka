a624dff05e2921953b2692f96610c78b5ca7160c
Mikko <mikko.vaaranmaki@gmail.com>
2018-04-06 17:35:57 +0300

    Välipalautus 3

--- ohsiha_harkka/app/models.py
+++ ohsiha_harkka/app/models.py
@@ -6,11 +6,6 @@ from datetime import date
 class TrafficLight(models.Model):
     name = models.TextField(primary_key=True)
     last_updated = models.TextField(blank=True, null=True)
-  #   idx = models.IntegerField(default="")
     status = models.TextField(default="")
-#    status = models.NullBooleanField(blank=True, null=True, default=None)
     coordinate_x = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=6)
     coordinate_y = models.DecimalField(blank=True, null=True, max_digits=8, decimal_places=6)
-
-#    def __str__(self):
-#        return self.name
